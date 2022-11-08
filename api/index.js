c124f000855eaf520c4906769875e573734aabdf
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-11-07 21:20:15 -0300

    Agregado vercel.json

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const sendEmail = require('./routes/send-email')
 
 const app = express()
 
+const PORT = process.env.PORT || 3000
+
 app.use(express.json())
 
 app.use('/', express.static((path.join(__dirname, '../frontend/build'))))
 
 app.use('/send-email', sendEmail)
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Sever on port ${process.env.PORT || 3000}`)
+app.listen(PORT, () => {
+  console.log(`Sever on port ${PORT}`)
 })
